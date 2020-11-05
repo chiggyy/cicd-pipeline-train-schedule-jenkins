feef4978ccfbef16ee221fe53f2e7521c73bc509
chirag <chirag@15bells.com>
2020-11-05 22:55:51 +0530

    app port back

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var trainsRouter = require('./routes/trains');
 
 var app = express();
-const port = 8000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/trains', trainsRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
